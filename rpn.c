ee4c029cb95007d0e25606ffc62aa8b19ec9291a
Jhanny-Kin <88437453+Jhanny-Kin@users.noreply.github.com>
2023-07-14 10:52:05 +0800

    clean up rpn.c

--- rpn.c
+++ rpn.c
@@ -1,111 +1,79 @@
 #include <stdio.h>
-#include <stdlib.h>
 #define MAX 256
 #define LEN 8
 
-typedef struct{
-    char body[MAX];
-    int top;
-}cstack;
-
-//this is a supid float stack, exact same as type 'cstack' except it stores float
+// float,int,char can be converted automatically
 typedef struct{
     float body[MAX];
     int top;
-}fstack;
-
+}stack;
 
 int main(void);
+void init(stack *);
+void push(stack *, float);
+float pop(stack *);
 int type(char);
-// c for char stack function
-void cinit(cstack *);
-void cpush(cstack *, char);
-char cpop(cstack *);
-// f for float stack funtion
-void finit(fstack *);
-void fpush(fstack *, float);
-float fpop(fstack *);
 
 
 int main(void){
+    stack oprts; init(&oprts);
     char *expr = "1.1+1-4*5/1+(4-1*9/1+9*8/1+0)";
-    printf("Original: [ %s ]\n", expr);
-
-    cstack oprts;
-    cinit(&oprts);
     char rpn[MAX][LEN] = {0};
     int row = 0;
     int level;
-    
-    while (*expr != 0){
-        // skip space
-        if(*expr==' '){
-            expr++;
-            continue;;
-        }
 
+    // convert to reverse polish notation
+    while (*expr != 0){
         level = type(*expr);
         switch (level){
-            case 0:
-                // output figures directly 
+            case '?':   // skip any unknown character
+                expr++;
+            case 0:     // read figures
                 for (int i=0; !type(*expr); ++i){
                     rpn[row][i] = *expr++;
                 }
                 ++row;
                 break;
-            
-            case -1:
-                // cpush all '('
+            case -1:    // push '(' or clear stack until top reach ')'
                 if (*expr == '('){
-                    cpush(&oprts, *expr++);
-                // cpop out all the operation signs until cstack top reaches the closest '('
+                    push(&oprts, *expr++);
                 } else{
                     while (oprts.body[oprts.top] != '('){
-                        rpn[row++][0] = cpop(&oprts);
+                        rpn[row++][0] = pop(&oprts);
                     }
-                    cpop(&oprts);
+                    pop(&oprts);
                     expr++;
                 }
                 break;
-
-            default:
-                // convert "-x" to "0-x"
+            default:    // case 2&3: clear stack until top is less prior than current sign, and push itself
                 if (*expr == '-' && (rpn[0][0]==0 || *(expr-1)=='(')){
-                    rpn[row++][0] = '0';
+                    rpn[row++][0] = '0';    // convert '-x' to '0-x'
                 }
-                // cpop out all the operation signs until it clear out cstack or find a sign that is less prior
                 while (oprts.top!=-1 && type(oprts.body[oprts.top]) >= level){
-                    rpn[row++][0] = cpop(&oprts);
+                    rpn[row++][0] = pop(&oprts);
                 }
-                cpush(&oprts, *expr++);
+                push(&oprts, *expr++);
                 break;
         }
     }
 
+    // output remainings in stack
     while (oprts.top != -1){
-        rpn[row++][0] = cpop(&oprts);
+        rpn[row++][0] = pop(&oprts);
     }
-    
-    printf("RPolishN: [ ");
-    for (int i=0; rpn[i][0]; ++i){
-        printf("%s ", rpn[i]);
-    }
-    putchar(']');
 
-
-    fstack calc;
-    finit(&calc);
+    stack calc; init(&calc);
     float number;
     int index;
     int deci;
 
+    // calculation
     for (int i=0; rpn[i][0]; ++i){
         index = rpn[i][0];
         number = 0.0;
+        deci = 0;
 
-        if (!type(index)){
-            deci = 0;
-            // read figure include decimals
+        if (!type(index)){  // read figure include decimals
             for (int c=0; rpn[i][c]!=0; c++){
                 if (rpn[i][c] == '.'){
                     deci = 1; continue;
@@ -113,49 +81,40 @@ int main(void){
                 number = number*10 + rpn[i][c] - 48;
                 if (deci) number /= 10;
             }
-            fpush(&calc, number);
-            
+            push(&calc, number);
         } else{
-            // calculations, some math manipulations for subtraction and division
+            // basic calculations with some math manipulations for subtraction and division
             switch (index){
-                case '+': fpush(&calc, fpop(&calc)+fpop(&calc)); break;
-                case '-': fpush(&calc, -fpop(&calc)+fpop(&calc)); break;
-                case '*': fpush(&calc, fpop(&calc)*fpop(&calc)); break;
-                case '/': fpush(&calc, 1/fpop(&calc)*fpop(&calc)); break;
+                case '+': push(&calc, pop(&calc)+pop(&calc)); break;
+                case '-': push(&calc, -pop(&calc)+pop(&calc)); break;
+                case '*': push(&calc, pop(&calc)*pop(&calc)); break;
+                case '/': push(&calc, 1/pop(&calc)*pop(&calc)); break;
             }
         }
     }
 
-    printf(" -> %.2f\n", fpop(&calc));
+    // print result
+    printf("RPolishN: [ ");
+    for (int i=0; rpn[i][0]; ++i){
+        printf("%s ", rpn[i]);
+    }
+    printf("] -> %.2f\n", pop(&calc));
+
     return 0;
 }
 
-// functions
-void cinit(cstack *skt){
-    skt->top = -1;
-}
-void finit(fstack *skt){
+
+void init(stack *skt){
     skt->top = -1;
 }
 
-
-void cpush(cstack *stk, char value){
-    stk->body[++stk->top] = value;
-}
-void fpush(fstack *stk, float value){
+void push(stack *stk, float value){
     stk->body[++stk->top] = value;
 }
 
-
-// cpop only make user overwrite it through cpush
-// do not manipulate stk.top anywhere
-char cpop(cstack *stk){
+float pop(stack *stk){
     return stk->body[stk->top--];
 }
-float fpop(fstack *stk){
-    return stk->body[stk->top--];
-}
-
 
 int type(char c){
     if (('0'<=c && c<='9') || c=='.'){
@@ -164,7 +123,9 @@ int type(char c){
         return 2;
     } else if (c=='+' || c=='-'){
         return 1;
-    } else{
+    } else if(c=='(' || c==')'){
         return -1;
+    }else{
+        return '?';
     }
 }
