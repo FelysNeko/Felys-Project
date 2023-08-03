4a0c71be9b0c86804a35bab7f4d0e308b11a9f9b
Jhanny-Kin <88437453+Jhanny-Kin@users.noreply.github.com>
2023-08-03 20:35:41 +0800

    Update parse.c

--- source/v1.0/parse.c
+++ source/v1.0/parse.c
@@ -55,6 +55,7 @@ cmdparse(cmd *home, container raw)
         cmd *top = dtoc(stktop(&end));
         cmd *jump = dtoc(stktop(&skip));
 
+        // if while, if, elif reuslt in FALSE, it jump to the next line that have the same scope it has
         if (top!=NULL && strmatch(top->type, WHILE) && top->scope==next->scope) {
             line->jump = top;
             top->jump = next;
@@ -64,12 +65,15 @@ cmdparse(cmd *home, container raw)
             stkpop(&end);
         }
 
+        // after finish if or elif, their last line will jump to the nearest cmd that have same scope as if or elif
+        // but the target line cannot have keyword elif or else
         if (!strmatch(line->type, ELIF) && !strmatch(line->type, ELSE)) {
             while (skip.top>-1 && jump!=NULL && line->scope==jump->scope-1) {
                 dtoc(stkpop(&skip))->jump = line;
             }
         }
         
+        // push if meet condition
         if (strmatch(line->type, IF) || strmatch(line->type, ELIF) || strmatch(line->type, WHILE)) {
             stkpush(&end, (long)line);
         } else if (next!=NULL && (strmatch(next->type, ELIF) || strmatch(next->type, ELSE))) {
@@ -97,14 +101,17 @@ static cmd *
 cmdread(cmd *prev, char *str, int index)
 {
     cmd *line = (cmd *)malloc(sizeof(cmd));
-    prev->next = line;
+    prev->next = line;      // link previous cmd to current cmd
     line->index = index+1;
     line->next = NULL;
 
+    // check indentations
     if ((line->scope=tab(str)) == -1) {
         error("IncorrectTabs", index+1);
     }
 
+    // when scope decrease, add an empty cmd to wrap this scope up
+    // it's very necessary, because it make interpreter find jump line easier
     for (int diff=prev->scope-line->scope; diff>0; --diff) {
         prev->next = (cmd *)malloc(sizeof(cmd));
         prev = prev->next;
@@ -112,13 +119,13 @@ cmdread(cmd *prev, char *str, int index)
         prev->type = NONE;
         prev->expr = NONE;
         line->jump = NULL;
-        prev->index = -1;
+        prev->index = -1;   // any cmd added by interpreter have index -1
         prev->next = line;
     }
 
     char keyword[LEN] = {0};
-    int end = loadkw(line->scope*4, str, keyword);
-    line->expr = str+end;
+    int end = loadkw(line->scope*4, str, keyword);  // see where keyword ends and copy it
+    line->expr = str+end;   // asume this cmd have keyword, yet the final result
 
     if (strmatch(keyword, IF)) {
         line->type = IF;
@@ -132,9 +139,11 @@ cmdread(cmd *prev, char *str, int index)
         line->type = EXIT;
     } else if(strmatch(keyword, GOTO)) {
         line->type = GOTO;
+    } else if(strmatch(keyword, SHOW)) {
+        line->type = SHOW;
     } else{
         line->type = NONE;
-        line->expr = str+line->scope*4;
+        line->expr = str+line->scope*4; // if no keyword detected, everything will be an expr
     }
 
     return line;
